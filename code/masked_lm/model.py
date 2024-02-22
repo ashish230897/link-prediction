0ca972723d624817c0832631bf3bc9fe8f222a2d
Meet Doshi <meetdoshi90@gmail.com>
2024-02-22 17:50:20 +0530

    Modified MLM attention name

--- code/masked_lm/model.py
+++ code/masked_lm/model.py
@@ -26,7 +26,7 @@ class NewGELU(nn.Module):
     def forward(self, x):
         return 0.5 * x * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (x + 0.044715 * torch.pow(x, 3.0))))
 
-class CausalSelfAttention(nn.Module):
+class All2AllAttention(nn.Module):
     """
     A vanilla multi-head masked self-attention layer with a projection at the end.
     It is possible to use torch.nn.MultiheadAttention here but I am including an
@@ -89,7 +89,7 @@ class Block(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.ln_1 = nn.LayerNorm(config.n_embd)
-        self.attn = CausalSelfAttention(config)
+        self.attn = All2AllAttention(config)
         self.ln_2 = nn.LayerNorm(config.n_embd)
         self.mlpf = FFN(config)
 
